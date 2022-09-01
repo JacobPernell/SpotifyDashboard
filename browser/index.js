9f7e62acfecb557310f23397415fc85486d36dd5
Jacob Pernell <jacob.pernell@gmail.com>
2022-09-01 13:10:17 -0700

    Updated localhost port numbers to correct ones

--- browser/index.js
+++ browser/index.js
@@ -3,7 +3,9 @@ import './style.css';
 const loginButton = document.getElementById('login');
 
 loginButton.addEventListener('click', () => {
-  fetch('/api/login')
+  fetch('http://127.0.0.1:8000/api/login', {
+    mode: 'no-cors',
+  })
     .then(response => response.text())
     .then(data => console.log('Data :', data))
     .catch(err => console.log('Error: ', err));
--- main.py
+++ main.py
@@ -5,16 +5,16 @@ import httpx
 app = FastAPI()
 
 CLIENT_ID = ''
-redirect_uri = 'http://localhost:8000/callback'
+redirect_uri = 'http://localhost:8080/callback'
 scope = 'user-read-private user-read-email'
 
 
 @app.get("/api/login")
 async def login():
-    # try:
-    #     params = {'response_type': 'code', 'client_id': CLIENT_ID, 'scope': scope, 'redirect_uri': redirect_uri, 'code_challenge_method': 'S256' }
-    #     r = httpx.get('https://accounts.spotify.com/authorize?', params=params)
-    #     return r.json()
-    # except:
-    #     print("something went wrong with httpx")
-    print("Hello from /api/login")
+    try:
+        params = {'response_type': 'code', 'client_id': CLIENT_ID, 'scope': scope, 'redirect_uri': redirect_uri, 'code_challenge_method': 'S256' }
+        r = httpx.get('https://accounts.spotify.com/authorize?', params=params)
+        return r.json()
+    except:
+        print("HTTPX error: ", r)
+        print(r.request)
